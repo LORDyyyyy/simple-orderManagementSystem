bd1a1b3db804cfa99dccb4d6b3f67d3d001a91cb
LORDyyyyy <mlord6271@gmail.com>
2024-06-20 16:43:06 +0300

    modified:   README.md

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import Config from './config/config';
+import Config from 'src/config';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
